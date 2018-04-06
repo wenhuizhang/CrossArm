8a15308f55ce7d23328647106167bcc42e8e3a9b
wenhuizhang <wenhui@gwmail.gwu.edu>
2018-04-06 02:23:14 -0400

    init

---
+++ src/i586-zephyr-elf/lib/gcc/i586-zephyr-elf/6.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/Volumes/CrossToolNG/build/.build/src/gcc-6.3.0/configure --build=x86_64-build_apple-darwin17.5.0 --host=x86_64-build_apple-darwin17.5.0 --target=i586-zephyr-elf --prefix=/Volumes/CrossToolNG/x-tools/i586-zephyr-elf --with-local-prefix=/Volumes/CrossToolNG/x-tools/i586-zephyr-elf/i586-zephyr-elf --without-headers --with-newlib --enable-threads=no --disable-shared --with-tune=pentium --with-pkgversion='crosstool-NG crosstool-ng-1.23.0' --enable-__cxa_atexit --disable-libgomp --disable-libmudflap --disable-libmpx --disable-libssp --disable-libquadmath --disable-libquadmath-support --with-gmp=/Volumes/CrossToolNG/build/.build/i586-zephyr-elf/buildtools --with-mpfr=/Volumes/CrossToolNG/build/.build/i586-zephyr-elf/buildtools --with-mpc=/Volumes/CrossToolNG/build/.build/i586-zephyr-elf/buildtools --with-isl=/Volumes/CrossToolNG/build/.build/i586-zephyr-elf/buildtools --enable-lto --enable-target-optspace --disable-nls --disable-multilib --enable-languages=c,c++";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "pentium" }, { "arch", "pentium" }, { "tune", "pentium" } };
